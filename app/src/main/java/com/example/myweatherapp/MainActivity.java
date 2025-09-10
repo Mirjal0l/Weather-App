26f2b1b12e370251eb27a52ceadd8f17a802929a
Mirjal0l <imomalievmirjalol0@gmali.com>
2025-09-10 09:50:55 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myweatherapp/MainActivity.java
@@ -0,0 +1,153 @@
+package com.example.myweatherapp;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView cityNameText, temperatureText, humidityText, descriptionText, windText;
+    private ImageView weatherIcon;
+    private Button refreshButton;
+    private EditText cityNameInput;
+    private ImageButton audioSearch;
+    private static final int REQUEST_CODE = 1;
+    private static final String API_KEY = "98477fe997518a24695b3b6baedba7d0";
+    private ImageButton info_btn;
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        audioSearch = findViewById(R.id.audioS);
+        cityNameText = findViewById(R.id.cityNameText);
+        temperatureText = findViewById(R.id.temperatureText);
+        humidityText = findViewById(R.id.humidityText);
+        windText = findViewById(R.id.windText);
+        descriptionText = findViewById(R.id.descriptionText);
+        weatherIcon = findViewById(R.id.weatherIcon);
+        refreshButton = findViewById(R.id.fetchWeatherButton);
+        cityNameInput = findViewById(R.id.cityNameInput);
+        info_btn = findViewById(R.id.info_btn);
+
+        info_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.info_dialog);
+                dialog.setCanceledOnTouchOutside(true);
+                if (dialog.getWindow() != null) {
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                }
+                dialog.show();
+            }
+        });
+
+        audioSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                speechIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Listening...");
+                startActivityForResult(speechIntent,REQUEST_CODE);
+            }
+        });
+
+
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String cityName = cityNameInput.getText().toString();
+                if (!cityName.isEmpty()) {
+                    fetchWeatherData(cityName);
+                } else {
+                    cityNameInput.setError("Please enter a city name");
+                }
+            }
+        });
+
+        //fetchWeatherData("Tashkent");
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent speechIntent) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            ArrayList<String> matches = speechIntent.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            cityNameInput.setText(matches.get(0).toString());
+        }
+        super.onActivityResult(requestCode, resultCode, speechIntent);
+    }
+
+    private void fetchWeatherData(String cityName) {
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + API_KEY + "&units=metric";
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(() -> {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(url).build();
+            try {
+                Response response = client.newCall(request).execute();
+                String result = response.body().string();
+                runOnUiThread(() -> updateUI(result));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void updateUI(String result) {
+        if (result != null) {
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONObject main = jsonObject.getJSONObject("main");
+                double temperature = main.getDouble("temp");
+                double humidity = main.getDouble("humidity");
+                double windSpeed = jsonObject.getJSONObject("wind").getDouble("speed");
+
+                String description = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
+                String iconCode = jsonObject.getJSONArray("weather").getJSONObject(0).getString("icon");
+                String resourceName = "ic_" + iconCode;
+                int resId = getResources().getIdentifier(resourceName, "drawable", getPackageName());
+                weatherIcon.setImageResource(resId);
+
+                cityNameText.setText(jsonObject.getString("name"));
+                temperatureText.setText(String.format("%.0f°", temperature));
+                humidityText.setText(String.format("%.0f%%", humidity));
+                windText.setText(String.format("%.0f km/h", windSpeed));
+                descriptionText.setText(description);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
